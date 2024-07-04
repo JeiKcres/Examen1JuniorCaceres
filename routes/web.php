d2a7a0fe1ccee5c1d6fabb362c864c99039af5ea
Junior Caceres <jrcaceres61@gmail.com>
2024-07-04 16:00:55 -0600

    Cambio solo de prueba

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use  App\Http\Controllers\HomeController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/prueba', [HomeController::class, 'prueba']);
